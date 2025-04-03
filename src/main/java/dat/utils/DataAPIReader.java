c30fde253be7a651308afd45e0f3c54426170440
AndyTheDragon <andre.jansam@gmail.com>
2025-04-03 16:11:00 +0200

    Cleaned up the APIReader class

--- src/main/java/dat/utils/DataAPIReader.java
+++ src/main/java/dat/utils/DataAPIReader.java
@@ -1,20 +1,24 @@
 package dat.utils;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import dat.exceptions.ApiException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class DataAPIReader
 {
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private final Logger logger = LoggerFactory.getLogger(DataAPIReader.class);
 
     public String getDataFromClient(String url)
     {
+
         try
         {
             HttpClient client = HttpClient.newHttpClient();
@@ -31,12 +35,13 @@ public class DataAPIReader
             }
             else
             {
+                logger.error("GET request failed. Status code: {}", response.statusCode());
                 throw new ApiException(response.statusCode(), "GET request failed. Status code: " + response.statusCode());
-                //System.out.println("GET request failed. Status code: " + response.statusCode());
             }
         }
-        catch (Exception e)
+        catch (InterruptedException | URISyntaxException  | IOException e)
         {
+            logger.error("Error fetching data from API", e);
             throw new RuntimeException("Error fetching data from API", e);
         }
     }
